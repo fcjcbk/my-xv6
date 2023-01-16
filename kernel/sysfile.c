bd9ae9b962575d0c70e18302c465955a0c77c778
Your Name <you@example.com>
2023-01-16 19:14:03 +0800

    完成实验Symbolic links

--- kernel/sysfile.c
+++ kernel/sysfile.c
@@ -322,6 +322,31 @@ sys_open(void)
     return -1;
   }
 
+  if(ip->type == T_SYMLINK && !(omode & O_NOFOLLOW)){
+      uint time = 0;
+      while(time < 10){
+        char path_name[MAXPATH];
+        if((readi(ip, 0, (uint64)path_name, 0, sizeof(path_name))) <= 0){
+          panic("open: readi");
+        }
+        iunlockput(ip);
+        if((ip = namei(path_name)) == 0){
+          end_op();
+          return -1;
+        }
+        ilock(ip);
+        if(ip->type != T_SYMLINK){
+          break;
+        }
+        time++;
+      }
+      if(time >= 10){
+        iunlockput(ip);
+        end_op();
+        return -1;
+      }
+    }
+
   if((f = filealloc()) == 0 || (fd = fdalloc(f)) < 0){
     if(f)
       fileclose(f);
@@ -482,5 +507,28 @@ sys_pipe(void)
     fileclose(wf);
     return -1;
   }
+  return 0;
+}
+
+uint64
+sys_symlink(void)
+{
+  char target[MAXPATH], path[MAXPATH];
+  struct inode *ip;
+  if(argstr(0, target, MAXPATH) < 0 || argstr(1,path, MAXPATH) < 0)
+    return -1;
+
+  begin_op();
+  ip = create(path, T_SYMLINK, 0, 0);
+  
+  if(ip == 0){
+    end_op();
+    return -1;
+  }
+  if(writei(ip, 0, (uint64)target, 0, sizeof(target)) != sizeof(target)){
+    panic("symlink: writei");
+  }
+  iunlockput(ip);
+  end_op();
   return 0;
 }
